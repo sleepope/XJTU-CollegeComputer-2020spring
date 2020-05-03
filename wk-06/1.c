2e9c6a602eb3c52237d63af3708a86be2eacd379
Sci-Zeal <SciZeal@outlook.com>
2020-05-03 15:27:38 +0800

    daily maintaince & wk6

---
+++ wk-06/1.c
@@ -0,0 +1,20 @@
+/*
+ * author: SciZeal
+ * email:  SciZeal@outlook.com
+ * time:   2020-03-23
+ *
+ * ALL COPYRIGHTS RESERVED
+ */
+
+#define __AUTHOR__IS__SCIZEAL__
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    printf("%g\n", (a + b + c) / 3.0);
+
+    return 0;
+}
