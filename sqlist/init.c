445a66535aa0e6c65cab13141923d90ea1cf7115
SciZeal <SciZeal@outlook.com>
2020-05-17 14:48:27 +0800

    wk13(finished)

--- sqlist/init.c
+++ sqlist/init.c
@@ -21,10 +21,13 @@ sqlist initSqList(int size)
 {
     sqlist list;
     list.head = (int *)malloc(size * sizeof(int));
+
+    // if failed
     if (!list.head)
     {
         printf("Initialization failed!\n");
     }
+
     list.length = 0;
     list.size = size;
     return list;
@@ -41,6 +44,64 @@ void lprintf(sqlist list)
     printf("\n");
 }
 
+// find element in the sequence list by sequential search
+int findElement(sqlist list, int element)
+{
+    for (int i = 0; i < list.length; i++)
+    {
+        if (element == list.head[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// add element into sequence list
+sqlist addElement(sqlist list, int element, int index)
+{
+    // if invalid index
+    if (index > list.length || index < 0)
+    {
+        printf("Invalid index!\n");
+        return list;
+    }
+
+    for (int i = list.length - 1; i >= index; i--)
+    {
+        list.head[i + 1] = list.head[i];
+    }
+    list.head[index] = element;
+    list.length++;
+    return list;
+}
+
+// remove element out of sequence list
+sqlist removeElement(sqlist list, int element)
+{
+    while (findElement(list, element) >= 0)
+    {
+        int index = findElement(list, element);
+        for (int i = index + 1; i < list.length; i++)
+        {
+            list.head[i - 1] = list.head[i];
+        }
+        list.length--;
+    }
+    return list;
+}
+
+// change element in the sequence list
+sqlist amendElement(sqlist list, int element, int newElement)
+{
+    int index = findElement(list, element);
+    if (index >= 0)
+    {
+        list.head[index] = newElement;
+    }
+    return list;
+}
+
 int main()
 {
     int size = 10;
@@ -49,10 +110,23 @@ int main()
     // fill elements
     for (int i = 0; i < size; i++)
     {
-        list.head[i] = i * i;
+        list.head[i] = i;
         list.length++;
     }
+    lprintf(list);
 
+    // add element
+    list = addElement(list, 100, 0);
+    list = addElement(list, 100, list.length);
+    lprintf(list);
+
+    // remove element
+    list = removeElement(list, 5);
+    list = removeElement(list, 100);
+    lprintf(list);
+
+    // amend element
+    list = amendElement(list, 6, 666);
     lprintf(list);
 
     return 0;
---
+++ wk-13/13-1.c
@@ -0,0 +1,92 @@
+/*
+ * author: SciZeal
+ * email:  SciZeal@outlook.com
+ * time:   2020-05-11
+ *
+ * ALL COPYRIGHTS RESERVED
+ */
+
+#define __AUTHOR__IS__SCIZEAL__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define maxn 100
+
+typedef struct SqList
+{
+    char *head;
+    int length;
+    int size;
+} sqlist;
+
+// initialize a sequence list with size
+sqlist initSqList(int size)
+{
+    sqlist list;
+    list.head = (char *)malloc(size * sizeof(char));
+    if (!list.head)
+    {
+        exit(0);
+    }
+    list.length = 0;
+    list.size = size;
+    return list;
+}
+
+// output a sequence list
+void lprintf(sqlist list)
+{
+    for (int i = 0; i < list.length; i++)
+    {
+        printf("%c", list.head[i]);
+    }
+    printf("\n");
+}
+
+// find element in the sequence list by sequential search
+int findElement(sqlist list, char element)
+{
+    for (int i = 0; i < list.length; i++)
+    {
+        if (element == list.head[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// remove element out of sequence list
+sqlist removeElement(sqlist list, char element)
+{
+    while (findElement(list, element) >= 0)
+    {
+        int index = findElement(list, element);
+        for (int i = index + 1; i < list.length; i++)
+        {
+            list.head[i - 1] = list.head[i];
+        }
+        list.length--;
+    }
+    return list;
+}
+
+int main()
+{
+    sqlist list = initSqList(maxn);
+    char c;
+    while ((c = getchar()) != '\n')
+    {
+        list.head[list.length++] = c;
+    }
+    lprintf(list);
+    printf("%d\n", list.length);
+
+    scanf("%c", &c);
+    list = removeElement(list, c);
+    lprintf(list);
+    printf("%d\n", list.length);
+
+    return 0;
+}
