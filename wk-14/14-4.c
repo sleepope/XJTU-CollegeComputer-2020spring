da91adf730075d002e0c1ed7a191d5e7a55d7508
SciZeal <SciZeal@outlook.com>
2020-05-22 02:08:57 +0800

    wk14(finished)

---
+++ wk-14/14-4.c
@@ -0,0 +1,45 @@
+/*
+ * author: SciZeal
+ * email:  SciZeal@outlook.com
+ * time:   2020-05-18
+ *
+ * MIT LICENCE
+ */
+
+#define __AUTHOR__IS__SCIZEAL__
+
+#include <stdio.h>
+
+int maxDivisor(int p, int q)
+{
+    while (q)
+    {
+        int r = p % q;
+        if (r == 0)
+        {
+            return q;
+        }
+        p = q;
+        q = r;
+    }
+    return -1;
+}
+
+int main()
+{
+    int p, q;
+    scanf("%d,%d", &p, &q);
+
+    // p >= q
+    if (p < q)
+    {
+        int tmp = p;
+        p = q;
+        q = tmp;
+    }
+
+    int maxDiv = maxDivisor(p, q);
+    printf("最大公约数是%d\n", maxDiv);
+    printf("最小公倍数是%d\n", p * q / maxDiv);
+    return 0;
+}
---
+++ wk-14/14-8.c
@@ -0,0 +1,70 @@
+/*
+ * author: SciZeal
+ * email:  SciZeal@outlook.com
+ * time:   2020-05-18
+ *
+ * MIT LICENCE
+ */
+
+#define __AUTHOR__IS__SCIZEAL__
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define maxn 100
+
+// judge if a number is prime number
+bool isPrime(int num)
+{
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// return reversed number in decimal
+int reversedPrime(char *num, int radix)
+{
+    int reversedNum = 0, len = strlen(num);
+    for (int i = 0; i < len; i++)
+    {
+        reversedNum += (num[i] - '0') * pow(radix, i);
+    }
+    return reversedNum;
+}
+
+int main()
+{
+    int prime, radix;
+    char num[maxn];
+
+    while (scanf("%d", &prime) == 1 && prime > 1)
+    {
+        scanf("%d", &radix);
+        if (isPrime(prime))
+        {
+            itoa(prime, num, radix);
+            if (isPrime(reversedPrime(num, radix)))
+            {
+                printf("Yes\n");
+            }
+            else
+            {
+                printf("No\n");
+            }
+        }
+        else
+        {
+            printf("No\n");
+        }
+    }
+
+    return 0;
+}
