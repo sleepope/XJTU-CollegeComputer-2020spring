61b8889e23720bd5980934ff2c33b5444ddb61c2
SciZeal <SciZeal@outlook.com>
2020-05-21 20:34:13 +0800

    wk14

---
+++ wk-14/14-6.c
@@ -0,0 +1,44 @@
+/*
+ * author: SciZeal
+ * email:  SciZeal@outlook.com
+ * time:   2020-05-18
+ *
+ * ALL COPYRIGHTS RESERVED
+ */
+
+#define __AUTHOR__IS__SCIZEAL__
+
+#include <stdio.h>
+
+#define maxn 20
+
+int arr[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int insert[maxn];
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", insert + i);
+    }
+    int pos;
+    scanf("%d", &pos);
+
+    printf("%d", *arr);
+    for (int i = 1; i < pos; i++)
+    {
+        printf(" %d", arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf(" %d", insert[i]);
+    }
+    for (int i = pos; i < 10; i++)
+    {
+        printf(" %d", arr[i]);
+    }
+
+    return 0;
+}
